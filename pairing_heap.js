df138027ee971cf6def522eca41c45812ff19b56
330k <kei.misawa@gmail.com>
2015-11-28 20:52:34 +0900

    Create pairing_heap.js

---
+++ pairing_heap.js
@@ -0,0 +1,73 @@
+/* Pairing Heap
+ * Copyright 2015 330k
+ * https://github.com/330k/
+ */
+function pairing_heap(){
+	var _root = null;
+	var _size = 0;
+	var _merge = function (i, j){
+		if(i === null) return j;
+		if(j === null) return i;
+		
+		if(i.p < j.p){
+			var ret = i;
+			i = j;
+			j = ret;
+		}
+		j.next = i.head;
+		i.head = j;
+		
+		return i;
+	};
+	var _mergeList = function (s){
+		var n = null;
+		
+		while(s !== null){
+			var a = s;
+			var b = null;
+			s = s.next;
+			a.next = null;
+			if(s !== null){
+				b = s;
+				s = s.next;
+				b.next = null;
+			}
+			a = _merge(a, b);
+			a.next = n;
+			n = a;
+		}
+		while(n !== null){
+			var j = n;
+			n = n.next;
+			s = _merge(j, s);
+		}
+		return s;
+	};
+	
+	var enqueue = function(priority, value){
+		_root = _merge(_root, {
+			p: priority,
+			v: value,
+			next: null,
+			head: null
+		});
+		_size++;
+	};
+	var dequeue = function(){
+		var result = _root.v;
+		_root = _mergeList(_root.head);
+		_size--;
+		
+		return result;
+	};
+	var size = function(){
+		return _size;
+	};
+	
+	return {
+		name: 'Pairing Heap',
+		enqueue: enqueue,
+		dequeue: dequeue,
+		size: size
+	};
+}
