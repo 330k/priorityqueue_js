af91a1d6f6c2badce85562d15f801513902146f7
330k <kei.misawa@gmail.com>
2017-09-20 23:05:21 +0900

    optimize test

--- binary_heap_proto.js
+++ binary_heap_proto.js
@@ -1,110 +1,105 @@
 /* Binary Heap
- * Copyright 2017 330k
+ * Copyright 2015 330k
  * https://github.com/330k/
  *
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  */
-function binary_heap_proto(){
+function binary_heap(){
 	"use strict";
-
-    if(!(this instanceof binary_heap_proto)){
-        return new binary_heap_proto();
-	}
-	
-	this._data = [];
-	this._size = 0;
-
-	return this;
-}
-
-binary_heap_proto.prototype.enqueue = function(priority, value){
-	var data = this._data;
-	var i = 0;
-	var p;
-	var ret = null;
-	
-	if(this._size){
-		data.push({p: priority, v: value});
-		this._size++;
-		i = this._size - 1;
-		p = (i - 1) >> 1;//Math.floor((i - 1) * 0.5);	// parent
-		while(p >= 0){
-			if(data[p].p < data[i].p){
-				ret = data[i];
-				data[i] = data[p];
-				data[p] = ret;
-			
-				i = p;
-				p = (i - 1) >> 1;//Math.floor((i - 1) * 0.5);
-			}else{
-				break;
-			}
-		}
-	}else{
-		data.push({p: priority, v: value});
-		this._size++;
-	}
-};
-
-binary_heap_proto.prototype.dequeue = function(){
-	var data = this._data;
-	var size = this._size;
-	var result = data[0].v;
-	var i = 0;
-	var c1 = 1;	// left child
-	var c2 = 2;	// right child
-	var p0;
-	var p1;
-	var p2;
-	var ret = null;
-	
-	data[0] = data[size - 1];
-	data.pop();
-	size--;
-	
-	while(c1 < size){
-		if(c2 < size){
-			p0 = data[i].p;
-			p1 = data[c1].p;
-			p2 = data[c2].p;
+	var _data = [];
+	var _size = 0;
+	var enqueue = function(priority, value){
+		var data = _data;
+		var i = 0;
+		var p = 0;
+		var ret = null;
 		
-			if((p1 < p2) && (p0 < p2)){
-				ret = data[i];
-				data[i] = data[c2];
-				data[c2] = ret;
-				i = c2;
-			}else if(p0 < p1){
-				ret = data[i];
-				data[i] = data[c1];
-				data[c1] = ret;
-				i = c1;
+		if(_size){
+			data.push({p: priority, v: value});
+			i = _size;
+			p = (i - 1) >> 1;//Math.floor((i - 1) * 0.5);	// parent
+			while(p >= 0){
+				if(data[p].p < data[i].p){
+					ret = data[i];
+					data[i] = data[p];
+					data[p] = ret;
+				
+					i = p;
+					p = (i - 1) >> 1;//Math.floor((i - 1) * 0.5);
+				}else{
+					break;
+				}
+			}
+		}else{
+			data.push({p: priority, v: value});
+		}
+		_size = (_size + 1) | 0;
+	};
+	var dequeue = function(){
+		var data = _data;
+		var size = _size - 1;
+		var result = data[0].v;
+		var i = 0;
+		var c1 = 1;	// left child
+		var c2 = 2;	// right child
+		var p0 = 0.0;
+		var p1 = 0.0;
+		var p2 = 0.0;
+		var ret = null;
+		
+		data[0] = data[size - 1];
+		data.pop();
+		
+		while(c1 < size){
+			if(c2 < size){
+				p0 = data[i].p;
+				p1 = data[c1].p;
+				p2 = data[c2].p;
+			
+				if((p1 < p2) && (p0 < p2)){
+					ret = data[i];
+					data[i] = data[c2];
+					data[c2] = ret;
+					i = c2;
+				}else if(p0 < p1){
+					ret = data[i];
+					data[i] = data[c1];
+					data[c1] = ret;
+					i = c1;
+				}else{
+					break;
+				}
+				c1 = (i << 1 + 1) | 0;
+				c2 = (i << 1 + 2) | 0;
 			}else{
+				p0 = data[i].p;
+				p1 = data[c1].p;
+				
+				if(p0 < p1){
+					ret = data[i];
+					data[i] = data[c1];
+					data[c1] = ret;
+				}
 				break;
 			}
-			c1 = i * 2 + 1;
-			c2 = i * 2 + 2;
-		}else{
-			p0 = data[i].p;
-			p1 = data[c1].p;
-			
-			if(p0 < p1){
-				ret = data[i];
-				data[i] = data[c1];
-				data[c1] = ret;
-			}
-			break;
 		}
-	}
+		
+		_size = size;
+		return result;
+	};
+	var top = function(){
+		return _data[0].v;
+	};
+	var size = function(){
+		return _size;
+	};
 	
-	this._size = size;
-	return result;
-};
-
-binary_heap_proto.prototype.top = function(){
-	return this._data[0].v;
-};
-binary_heap_proto.prototype.size = function(){
-	return this._size;
-};
-binary_heap_proto.prototype.name = "Binary Heap (Prototype)";
+	return {
+		name: 'Binary Heap',
+		enqueue: enqueue,
+		dequeue: dequeue,
+		top: top,
+		size: size
+	};
+}
---
+++ binomial_heap.js
@@ -0,0 +1,63 @@
+/* Binomial Heap
+ * Copyright 2017 330k
+ * https://github.com/330k/
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+function binomial_heap(){
+	"use strict";
+	var _root = null;
+	var _size = 0;
+	
+	var _merge = function(i, j){
+		var ret;
+
+		if(i === null) return j;
+		if(j === null) return i;
+		
+		if(i.p < j.p){
+			ret = i;
+			i = j;
+			j = ret;
+		}
+		i.right = _merge(i.right, j);
+		ret = i.right;
+		i.right = i.left;
+		i.left = ret;
+		
+		return i;
+	};
+	
+	var enqueue = function(priority, value){
+		_root = _merge(_root, {
+			p: priority,
+			v: value,
+			left: null,
+			right: null
+		});
+		_size++;
+	};
+	var dequeue = function(){
+		var result = _root.v;
+		
+		_root = _merge(_root.left, _root.right);
+		_size--;
+		
+		return result;
+	};
+	var top = function(){
+		return _root.v;
+	};
+	var size = function(){
+		return _size;
+	};
+	
+	return {
+		name: 'Binamial Heap',
+		enqueue: enqueue,
+		dequeue: dequeue,
+		top: top,
+		size: size
+	};
+}
